1e6737aee6559c286b5847353f4a9f6ef1e50239
satyam91s <satyamdubey210061@acropolis.in>
2024-12-22 00:26:51 +0530

    RestController

--- src/main/java/com/security/project_security/Controller/Mycon.java
+++ src/main/java/com/security/project_security/Controller/Mycon.java
@@ -1,5 +1,8 @@
 package com.security.project_security.Controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Mycon {
     
 }
---
+++ src/main/java/com/security/project_security/Security/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.security.project_security.Security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    
+}
