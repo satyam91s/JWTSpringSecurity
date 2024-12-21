16bb702a1b41bffe716f92c7af71ec980215bd77
satyam91s <satyamdubey210061@acropolis.in>
2024-12-22 01:00:22 +0530

    add SecurityConfig File

--- src/main/java/com/security/project_security/Controller/Mycon.java
+++ src/main/java/com/security/project_security/Controller/Mycon.java
@@ -1,8 +1,32 @@
 package com.security.project_security.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/home")
 public class Mycon {
+
+@GetMapping("/general")
+ public ResponseEntity<String> generalUser(){
+    return ResponseEntity.ok("I am general User");
+  }
+
+@GetMapping("/public")
+ public ResponseEntity<String> publicUser(){
+    return ResponseEntity.ok("I am Public User");
+  }
+
+@GetMapping("/admin")
+ public ResponseEntity<String> Admin(){
+    return ResponseEntity.ok("I am Admin");
+  }
+    
+
+
+
     
 }
--- src/main/java/com/security/project_security/Security/SecurityConfig.java
+++ src/main/java/com/security/project_security/Security/SecurityConfig.java
@@ -1,8 +1,29 @@
 package com.security.project_security.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
+    
+    @Bean
+public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+    httpSecurity
+        .csrf(csrf -> csrf.disable()) // Disable CSRF
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/home/public").permitAll() // Public endpoint
+            .anyRequest().authenticated() // All other requests need authentication
+        )
+        .formLogin(form -> form
+            // .loginPage("/login") // Customize login page if needed
+            .permitAll()
+        );
+
+    return httpSecurity.build();
+}
     
 }
