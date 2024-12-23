9fa1be5564eeccb4239f1933ca5dd076e308cdb9
satyam91s <satyamdubey210061@acropolis.in>
2024-12-23 22:21:31 +0530

    changes

--- src/main/java/com/security/project_security/Beans/User.java
+++ src/main/java/com/security/project_security/Beans/User.java
@@ -1,5 +1,10 @@
 package com.security.project_security.Beans;
 
 public class User {
+
+    public static Object withUsername(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'withUsername'");
+    }
     
 }
--- src/main/java/com/security/project_security/Security/SecurityConfig.java
+++ src/main/java/com/security/project_security/Security/SecurityConfig.java
@@ -3,11 +3,44 @@ package com.security.project_security.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.security.project_security.Beans.User;
 
 
 @Configuration
 public class SecurityConfig {
+
+//   @Bean
+// public UserDetailsService userDetailsService() {
+
+//     // // Define General User
+//     // UserDetails generalUser = User
+//     //     .withUsername("satyam")
+//     //     .password("{noop}1234") // Use {noop} for plain-text passwords
+//     //     .roles("NormalUser")
+//     //     .build();
+
+//     // // Define Admin User
+//     // UserDetails adminUser = User
+//     //     .withUsername("admin")
+//     //     .password("{noop}admin123") // Use {noop} for plain-text passwords
+//     //     .roles("Admin")
+//     //     .build();
+
+//     // // Define Public User
+//     // UserDetails publicUser = User
+//     //     .withUsername("public")
+//     //     .password("{noop}public123") // Use {noop} for plain-text passwords
+//     //     .roles("PublicUser")
+//     //     .build();
+
+//     // Add all users to the in-memory store
+//     return new InMemoryUserDetailsManager(generalUser, adminUser, publicUser);
+// }
     
     @Bean
 public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
