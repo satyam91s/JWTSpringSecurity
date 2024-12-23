508d23e3364a3c418e03c515492435f1c4b8c079
satyam91s <satyamdubey210061@acropolis.in>
2024-12-23 23:34:44 +0530

    upgradation

--- src/main/java/com/security/project_security/Beans/User.java
+++ src/main/java/com/security/project_security/Beans/User.java
@@ -2,9 +2,6 @@ package com.security.project_security.Beans;
 
 public class User {
 
-    public static Object withUsername(String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'withUsername'");
-    }
+    
     
 }
